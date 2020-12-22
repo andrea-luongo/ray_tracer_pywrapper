817659a3bcef87672717e699aaf209ef46d76058
WIN\aluo <aluo@dtu.dk>
2020-12-22 15:12:55 +0100

    working on BVH!

--- sources/BVH.cpp
+++ sources/BVH.cpp
@@ -8,4 +8,42 @@ BVH::BVH(const std::vector<std::shared_ptr<MyStructures::Primitive>>& p, int max
 	std::vector<BVHPrimitiveInfo> primitiveInfo(primitives.size());
 	for (size_t i = 0; i < primitives.size(); i++)
 		primitiveInfo[i] = {i, primitives[i]->BBox()};
+	int totalNodes = 0;
+	std::vector<std::shared_ptr<MyStructures::Primitive>> orderedPrims;
+	BVHBuildNode* root;
+	if (splitMethod == SplitMethod::HLBVH)
+		root = HLBVHBuild(primitiveInfo, &totalNodes, orderedPrims);
+	else
+		root = recursiveBuild(primitiveInfo, 0, primitives.size(), &totalNodes, orderedPrims);
+}
+
+BVHBuildNode* BVH::recursiveBuild(std::vector<BVHPrimitiveInfo>& primitiveInfo, int start, int end, int* totalNodes, std::vector<std::shared_ptr<MyStructures::Primitive>>& orderedPrims)
+{
+	BVHBuildNode* node = new BVHBuildNode();
+	(*totalNodes)++;
+	MyStructures::BBox bounds;
+	for (int i = start; i < end; i++) {
+		bounds = MyStructures::BBox::Union(bounds, primitiveInfo[i].bounds);
+	}
+	int nPrimitives = end - start;
+	if (nPrimitives == 1){
+		// create leaf bvhbuildnode
+		int firstPrimOffset = orderedPrims.size();
+		for (int i = start; i < end; i++) {
+			int primNum = primitiveInfo[i].primitiveNumber;
+			orderedPrims.push_back(primitives[primNum]);
+		}
+		node->initLeaf(firstPrimOffset, nPrimitives, bounds);
+		return node;
+	}
+	else {
+		// compute bound of primitive centroid, choose split dimension
+		MyStructures::BBox centroidBounds;
+		for (int i = start; i < end; i++) {
+			centroidBounds = MyStructures::BBox::Union(centroidBounds, primitiveInfo[i].centroid);
+		}
+		int dim = centroidBounds.MaximumExtent();
+		// partition primitives into two sets and build children
+	}
+	return node;
 }
--- sources/BVH.h
+++ sources/BVH.h
@@ -12,6 +12,28 @@ struct BVHPrimitiveInfo
 	BVHPrimitiveInfo(size_t primitiveNumber, const MyStructures::BBox& bounds) : primitiveNumber(primitiveNumber), bounds(bounds), centroid(0.5f * bounds.pMin + 0.5f * bounds.pMax) {};
 };
 
+struct BVHBuildNode
+{
+	MyStructures::BBox bounds;
+	BVHBuildNode* children[2];
+	int splitAxis, firstPrimOffset, nPrimitives;
+	void initLeaf(int first, int n, const MyStructures::BBox& b)
+	{
+		firstPrimOffset = first;
+		nPrimitives = n;
+		bounds = b;
+		children[0] = children[1] = nullptr;
+	}
+	void initInterior(int axis, BVHBuildNode* c0, BVHBuildNode* c1)
+	{
+		children[0] = c0;
+		children[1] = c1;
+		bounds = MyStructures::BBox::Union(c0->bounds, c1->bounds);
+		splitAxis = axis;
+		nPrimitives = 0;
+	}
+};
+
 class BVH
 {
 private:
@@ -20,4 +42,8 @@ private:
 	const SplitMethod splitMethod;
 public:
 	BVH(const std::vector<std::shared_ptr<MyStructures::Primitive>>& p, int maxPrimsInNode, SplitMethod splitMethod);
+
+protected:
+	BVHBuildNode* HLBVHBuild(const std::vector<BVHPrimitiveInfo>& primitiveInfo, int* totalNodes, std::vector<std::shared_ptr<MyStructures::Primitive>>& orderedPrims);
+	BVHBuildNode* recursiveBuild(std::vector<BVHPrimitiveInfo>& primitiveInfo, int start, int end, int* totalNodes, std::vector<std::shared_ptr<MyStructures::Primitive>>& orderedPrims);
 };
