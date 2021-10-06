b7e915ec95a1ec98a15bc3a916eabdca2d7043d1
andrea-luongo <diochocofi1!>
2021-10-06 14:52:33 +0200

    updated project

--- sources/ctb_main.cpp
+++ sources/ctb_main.cpp
@@ -7,20 +7,20 @@
 #include <ppl.h>
 namespace py = pybind11;
 
-void add_rep(uint8_t grey, uint32_t stride, uint8_t color, uint32_t& bits_on, std::vector<uint8_t>& rle)
+void add_rep(uint8_t gray7, uint32_t stride, uint32_t& bits_on, std::vector<uint8_t>& rle)
 {
     if (stride == 0)
         return;
-    if (grey > 0)
+    if (gray7 > 0)
     {
         bits_on += stride;
     }
-    else if (grey > 1)
+    if (stride > 1)
     {
-        grey |= uint8_t(0x80);
+        gray7 |= uint8_t(0x80);
     }
-    rle.push_back(grey);
-    if (grey <= 1)
+    rle.push_back(gray7);
+    if (stride <= 1)
     {
         return;
     }
@@ -49,74 +49,57 @@ void add_rep(uint8_t grey, uint32_t stride, uint8_t color, uint32_t& bits_on, st
 }
 
 
-py::tuple rle_encode_graymap(py::array_t<uint8_t>& grey_array)
+py::array_t<int32_t> RGBA(py::array_t<int32_t>& pic)
 {
-    uint8_t color = 0xff;
-    uint32_t stride = 0;
+    py::buffer_info buf1 = pic.request();
+    py::array_t<int32_t> result = py::array_t<int32_t>(buf1.size);
+
+    py::buffer_info buf2 = result.request();
+    int32_t* ptr1 = (int32_t*)buf1.ptr,
+        * ptr2 = (int32_t*)buf2.ptr;
+    int X = buf1.shape[0];
+    int Y = buf1.shape[1];
+    int Z = buf1.shape[2];
+    for (size_t idx = 0; idx < X; idx++) {
+        for (size_t idy = 0; idy < Y; idy++) {
+            for (size_t idz = 0; idz < Z; idz++) {
+                ptr2[idx * Y * Z + idy * Z + idz] = ptr1[idx * Y * Z + idy * Z + idz] | (ptr1[idx * Y * Z + idy * Z + idz] << 8);
+            }
+        }
+    }
+    // reshape array to match input shape
+    result.resize({ X,Y,Z });
+
+    return result;
+    
+}
+
+
+py::tuple rle_encode_graymap(py::array_t<uint8_t>& grey_array)
+//py::tuple rle_encode_graymap(py::array_t<uint8_t>& grey_array)
+{
+    uint8_t color(0xff);
+    uint32_t stride(0);
     std::vector<uint8_t> rle;
-    uint32_t bits_on = 0;
-    std::cout << grey_array << std::endl;
-    std::cout << grey_array.size() << std::endl;
-    std::cout << grey_array.dtype() << std::endl;
+    uint32_t bits_on(0);
     for (int idx = 0; idx < grey_array.size(); idx++)
     {
-        uint8_t grey = grey_array.at(idx);
-        std::cout << grey << std::endl;
-        if (grey == color)
+        uint8_t gray7 = grey_array.at(idx);
+        if (gray7 == color)
         {
-            stride += 1;
-            std::cout << "stride " << stride << std::endl;
+            stride += uint8_t(1);
         }
         else 
         {
-            add_rep(grey, stride, color, bits_on, rle);
-            color = grey;
+            add_rep(color, stride, bits_on, rle);
+            color = gray7;
             stride = 1;
-
-            std::cout << "stride " << stride << std::endl;
-            std::cout << "stride " << color << std::endl;
         }
     }
-    return py::make_tuple(py::cast(rle) , bits_on);
+    add_rep(color, stride, bits_on, rle);
+    //return py::make_tuple(rle , bits_on);
+    return py::make_tuple(py::array_t<uint8_t>(rle.size(), rle.data()), bits_on);
 }
-
-//#     def add_rep(gray7: np.uint8, stride : np.uint) :
-//    #         nonlocal bits_on, rle
-//#         if stride == 0:
-//    #             return
-//#         if gray7 > 0:
-//    #             bits_on += stride
-//#         if stride > 1:
-//    #             gray7 |= np.uint8(0x80)
-//    #         rle.append(gray7)
-//#         if stride <= 1:
-//    #             _ = 0
-//#         elif stride <= 0x7f:
-//    #             rle.append(np.uint8(stride))
-//#         elif stride <= 0x3fff:
-//    #             rle.append(np.uint8(stride >> 8) | np.uint8(0x80))
-//    #             rle.append(np.uint8(stride))
-//#         elif stride <= 0x1fffff:
-//    #             rle.append(np.uint8(stride >> 16) | np.uint8(0xc0))
-//    #             rle.append(np.uint8(stride >> 8))
-//    #             rle.append(np.uint8(stride))
-//#         elif stride <= 0xfffffff:
-//    #             rle.append(np.uint8(stride >> 24) | np.uint8(0xe0))
-//    #             rle.append(np.uint8(stride >> 16))
-//    #             rle.append(np.uint8(stride >> 8))
-//    #             rle.append(np.uint8(stride))
-//#
-//    #     def build_rle() :
-//    #         nonlocal stride, color
-//    #         for y in range(size[0]) :
-//    #             for x in range(size[1]) :
-//    #                 grey7 = grey7_matrix[y, x]
-//#                 if grey7 == color:
-//    #                     stride += 1
-//#                 else:
-//    #                     add_rep(color, stride)
-//    #                     color = grey7
-//    #                     stride = 1
 
 
 PYBIND11_MODULE(ctbConverterPyWrapper, m) {
@@ -124,6 +107,7 @@ PYBIND11_MODULE(ctbConverterPyWrapper, m) {
         Pybind wrapper for .ctb conversion
     )pbdoc";
     m.def("rle_encode_graymap", &rle_encode_graymap);
+    m.def("RGBA", &RGBA);
   
     
 #ifdef VERSION_INFO
